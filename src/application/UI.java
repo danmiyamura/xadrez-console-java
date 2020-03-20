51fbdef2005e5a70bfa7cca6b2ce4073e718a38c
Danilo Miyamura <danilo.miyamura@fatec.sp.gov.br>
2020-03-20 15:49:04 -0300

    Limpa tela e tratamento de exceções

--- src/application/UI.java
+++ src/application/UI.java
@@ -33,6 +33,11 @@ public class UI {
 
 	// ------------------------------------------------------------------------------------------------------
 
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 			String s = sc.nextLine();
