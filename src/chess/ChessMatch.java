e07cea7c1aa5b19fd84fb2de287f54eb27fcce50
Danilo Miyamura <danilo.miyamura@fatec.sp.gov.br>
2020-03-20 21:23:50 -0300

    Possíveis movimentos de uma peça

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -37,6 +37,9 @@ public class ChessMatch {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("Não existe uma peça na posição.");
 		}
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("Nao existe movimentos possiveis para a peca escolhida.");
+		}
 	}
 	
 	private Piece makeMove(Position source, Position target) {
